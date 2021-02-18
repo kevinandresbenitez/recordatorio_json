d6fedc37066b6ff8bbbbe811080f54c49588648a
Kevin Benitez <kevinescudoderoble@gmail.com>
2021-02-18 15:52:19 -0300

    Aun sin ajustar

--- js/index.js
+++ js/index.js
@@ -1,7 +1,67 @@
 $(document).ready(function () {
-  console.log("hola")
+
   $.get("../info.json", function(data){
-    console.log(data);
+
+
+      var eventos_futuros=[];
+      var eventos_pasados=[];
+
+      data.eventos.forEach(arreglo => {
+
+        if(arreglo.fecha > data.fechaActual){
+            eventos_futuros.push(arreglo)
+        }
+        else if(arreglo.fecha < data.fechaActual){
+            eventos_pasados.push(arreglo)
+        }
+      });
+
+      eventos_futuros = eventos_futuros.sort(function(x,y){
+        if (x.fecha < y.fecha){
+          return 1;
+        }
+        return -1;
+      });
+      eventos_pasados= eventos_pasados.sort(function(x,y){
+        if (x.fecha < y.fecha){
+          return 1;
+        }
+        return -1;
+      });
+
+
+
+      for (var sec=0;eventos_futuros.length > sec && sec > 3;sec ++){
+        $("#cont_recientes").append(`
+
+        <div class="col-12 m-md-auto mb-3 mb-md-auto col-md-5 bg-light border border-dark rounded p-0 py-3">
+        <div class="col-12 border-bottom border-dark  ">
+          <a href=""><h3 class="mx-auto ">${eventos_futuros[sec].nombre}</h3></a>
+        </div>
+
+        <div class="col-12 py-2 text-justify">
+          <p>${eventos_futuros[sec].descripcion}</p>
+          <p> <small class="text-muted">${eventos_futuros[sec].fecha}</small></p>
+        </div>
+
+      </div>
+
+
+
+      `)}
+
+
+      $("#cont_pasados").append(`Hola`)
+
+
+      
+
+
+
+
+
+
+
   })
 
 });
